79f0ba930ba58ba220ffc4f33c7b5b3a1219a878
zz <531818100@qq.com>
2016-08-05 11:34:37 +0800

    add currentImageView
    获取当前视频截图

--- LFLiveKit/LFLiveSession.h
+++ LFLiveKit/LFLiveSession.h
@@ -87,6 +87,7 @@
 /*** The warterMarkView control whether the watermark is displayed or not ,if set ni,will remove watermark,otherwise add. set alpha represent mix *.*/
 @property (nonatomic, strong) UIView *warterMarkView;
 
+@property (nonatomic, strong) UIImageView *currentImageView;
 #pragma mark - Initializer
 ///=============================================================================
 /// @name Initializer

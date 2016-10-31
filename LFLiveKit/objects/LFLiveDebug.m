a8a160324e006461daa73a694612f396779329fd
ftao <ft@fts-book.local>
2016-10-31 11:33:45 +0800

    优化更新LFLiveKit中的部分注释错误、添加LFDebug的description方法

--- LFLiveKit/objects/LFLiveDebug.m
+++ LFLiveKit/objects/LFLiveDebug.m
@@ -10,5 +10,9 @@
 
 @implementation LFLiveDebug
 
+- (NSString *)description {
+    return [NSString stringWithFormat:@"丢掉的帧数:%ld 总帧数:%ld 上次的音频捕获个数:%d 上次的视频捕获个数:%d 未发送个数:%ld 总流量:%0.f",_dropFrame,_totalFrame,_currentCapturedAudioCount,_currentCapturedVideoCount,_unSendCount,_dataFlow];
+}
+
 
 @end

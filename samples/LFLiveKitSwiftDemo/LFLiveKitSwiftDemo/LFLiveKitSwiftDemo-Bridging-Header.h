565453fe9e5653b115b3f78e9091c96e3424231f
chenliming <chenliming@tudou.com>
2016-11-09 17:13:19 +0800

    support swift3

---
+++ samples/LFLiveKitSwiftDemo/LFLiveKitSwiftDemo/LFLiveKitSwiftDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LFLiveKitSwiftDemo-Bridging-Header.h
+//  LFLiveKitSwiftDemo
+//
+//  Created by admin on 2016/11/9.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#ifndef LFLiveKitSwiftDemo_Bridging_Header_h
+#define LFLiveKitSwiftDemo_Bridging_Header_h
+
+#import <LFLiveKit/LFLiveKit.h>
+
+#endif /* LFLiveKitSwiftDemo_Bridging_Header_h */

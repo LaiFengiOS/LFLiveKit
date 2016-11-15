140a81243698c6f3cb9ebcccf269f58748c26a85
LinBq <bq_lin@hotmail.com>
2016-11-15 10:21:16 +0800

    添加LFLiveDebug对象的description

--- LFLiveKit/objects/LFLiveDebug.m
+++ LFLiveKit/objects/LFLiveDebug.m
@@ -10,5 +10,18 @@
 
 @implementation LFLiveDebug
 
+- (NSString *)description{
+	NSString *desc = [NSString stringWithFormat:@"\nstreamId = %@, uploadUrl = %@, videoSize = %@, isRtmp = %@;\n\
+					  elapsedMilli = %.02f, timeStamp = %.02f, dataFlow = %.02f, bandwidth = %.02f, currentBandwidth = %.02f;\n\
+					  dropFrame = %ld, totalFrame = %ld;\n\
+					  capturedAudioCount = %ld, capturedVideoCount = %ld, currentCapturedAudioCount = %ld, currentCapturedVideoCount = %ld;\n\
+					  unSendCount = %ld\n",
+					  _streamId, _uploadUrl, NSStringFromCGSize(_videoSize), @(_isRtmp),
+					  _elapsedMilli, _timeStamp, _dataFlow, _bandwidth, _currentBandwidth,
+					  _dropFrame, _totalFrame,
+					  _capturedAudioCount, _capturedVideoCount, _currentCapturedAudioCount, _currentCapturedVideoCount,
+					  _unSendCount];
+	return desc;
+}
 
 @end

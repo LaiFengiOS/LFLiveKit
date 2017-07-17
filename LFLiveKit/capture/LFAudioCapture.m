048fd9e21877813db84ffa12cf5c105bbb894ecf
Racing <racing@17.media>
2017-07-17 11:08:47 +0800

    Echo cancelation

--- LFLiveKit/capture/LFAudioCapture.m
+++ LFLiveKit/capture/LFAudioCapture.m
@@ -45,8 +45,8 @@ NSString *const LFAudioComponentFailedToCreateNotification = @"LFAudioComponentF
         
         AudioComponentDescription acd;
         acd.componentType = kAudioUnitType_Output;
-        //acd.componentSubType = kAudioUnitSubType_VoiceProcessingIO;
-        acd.componentSubType = kAudioUnitSubType_RemoteIO;
+        acd.componentSubType = kAudioUnitSubType_VoiceProcessingIO;
+//        acd.componentSubType = kAudioUnitSubType_RemoteIO;
         acd.componentManufacturer = kAudioUnitManufacturer_Apple;
         acd.componentFlags = 0;
         acd.componentFlagsMask = 0;

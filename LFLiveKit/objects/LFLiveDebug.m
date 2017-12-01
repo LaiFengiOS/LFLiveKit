f477210cb42d04ecd274d232e1eecce5287fb576
Superbil <superbil@gmail.com>
2017-12-02 05:25:52 +0800

    Update LFLiveDebug message to english

--- LFLiveKit/objects/LFLiveDebug.m
+++ LFLiveKit/objects/LFLiveDebug.m
@@ -36,8 +36,13 @@
 }
 
 - (NSString *)description {
-    return [NSString stringWithFormat:@"丢掉的帧数:%ld 总帧数:%ld 上次的音频捕获个数:%d 上次的视频捕获个数:%d 未发送个数:%ld 总流量:%0.f",_dropFrame,_totalFrame,_currentCapturedAudioCount,_currentCapturedVideoCount,_unSendCount,_dataFlow];
+    return [NSString stringWithFormat:@"DropFrame:%ld TotalFrame:%ld CurrentCaptureAuidoCount:%zd CurrentCaptureVideoCount:%zd unSendCount:%ld TotalTraffic:%0.f",
+            _dropFrame,
+            _totalFrame,
+            (long)_currentCapturedAudioCount,
+            _currentCapturedVideoCount,
+            _unSendCount,
+            _dataFlow];
 }
-
 
 @end

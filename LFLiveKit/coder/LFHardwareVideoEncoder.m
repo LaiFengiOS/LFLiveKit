6fa16af68c04ecfbc85852500fa3baed37581712
yu840915 <yu840915@gmail.com>
2022-03-22 12:09:16 +0800

    Turn off b frame

--- LFLiveKit/coder/LFHardwareVideoEncoder.m
+++ LFLiveKit/coder/LFHardwareVideoEncoder.m
@@ -73,7 +73,7 @@ static NSTimeInterval const kSessionLockDuration = 2.0f;
     VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_DataRateLimits, (__bridge CFArrayRef)limit);
     VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_RealTime, kCFBooleanTrue);
     VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_H264_Main_AutoLevel);
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AllowFrameReordering, kCFBooleanTrue);
+    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AllowFrameReordering, kCFBooleanFalse);
     VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_H264EntropyMode, kVTH264EntropyMode_CABAC);
     VTCompressionSessionPrepareToEncodeFrames(compressionSession);
     
